bc337e113ee33627a2d6a5b440560336259180f5
jpchato <jpchato@gmail.com>
2020-12-17 14:46:50 -0800

    added date picker

--- natureShare/natureShareApp/forms.py
+++ natureShare/natureShareApp/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import *
+from django.forms import ModelForm
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class OrganismForm(forms.ModelForm):
     class Meta:
         model = Organism
         fields = ['picture', 'name', 'edibility', 'ecosystem', 'weather', 'date', 'location', 'user']
         exclude = ['user']
+        widgets  = {
+            'date': DateInput()
+        }
