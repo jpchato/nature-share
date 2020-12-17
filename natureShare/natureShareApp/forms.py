29dde5a4846fec422cf05c57a68e4065205f779c
jpchato <jpchato@gmail.com>
2020-12-17 15:15:33 -0800

    home page styling

--- natureShare/natureShareApp/forms.py
+++ natureShare/natureShareApp/forms.py
@@ -5,6 +5,8 @@ from django.forms import ModelForm
 class DateInput(forms.DateInput):
     input_type = 'date'
 
+# https://stackoverflow.com/a/35968816/14263621
+
 class OrganismForm(forms.ModelForm):
     class Meta:
         model = Organism
