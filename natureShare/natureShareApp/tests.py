d0e72f74ef9e10512a7885135bda52d6c5e81c54
jpchato <jpchato@gmail.com>
2020-12-23 11:20:10 -0800

    drafting tests

--- natureShare/natureShareApp/tests.py
+++ natureShare/natureShareApp/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from natureShareApp.models import Organism
 
-# Create your tests here.
+# class OrganismTestCase(TestCase):
+#     def setUp(self):
+#         Organism.objects.create(name="lion", sound="roar")
+#         Organism.objects.create(name="cat", sound="meow")
+
+#     def test_Organisms_can_speak(self):
+#         """Organisms that can speak are correctly identified"""
+#         lion = Organism.objects.get(name="lion")
+#         cat = Organism.objects.get(name="cat")
+#         self.assertEqual(lion.speak(), 'The lion says "roar"')
+#         self.assertEqual(cat.speak(), 'The cat says "meow"')
