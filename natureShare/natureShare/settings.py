2931f2a0914eb285df0be0df45de3667517d6539
jpchato <jpchato@gmail.com>
2021-01-06 11:38:36 -0800

    basic set up for heroku deployment

--- natureShare/natureShare/settings.py
+++ natureShare/natureShare/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -141,3 +142,4 @@ LOCATION_FIELD = {
 'provider.google.map.type': 'ROADMAP',
 }
 
+django_heroku.settings(locals())
