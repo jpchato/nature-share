df230df21732e84fc0cee5949fa7f198805dc07d
jpchato <jpchato@gmail.com>
2021-01-08 12:23:02 -0800

    fixed edit and delete urls

--- natureShare/natureShareApp/urls.py
+++ natureShare/natureShareApp/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     # https://learndjango.com/tutorials/django-search-tutorial
     path('search/', SearchResultsView.as_view(), name='search_results'),
     path('search/', UserSearchResultsView.as_view(), name='user_search_results'),
-    path('<pk>/update', OrganismUpdate.as_view()),
-    path('<pk>/delete', OrganismDelete.as_view()),
+    path('<pk>/update', OrganismUpdate.as_view(), name='edit'),
+    path('<pk>/delete', OrganismDelete.as_view(), name='delete'),
     path('user_organisms', views.user_organisms, name = 'user_organisms')
    
     
