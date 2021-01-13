6c337fca24759d982120fbb54fe5b0edd8c28b04
jpchato <jpchato@gmail.com>
2021-01-13 12:25:36 -0800

    fixed settings for development

--- natureShare/natureShare/settings.py
+++ natureShare/natureShare/settings.py
@@ -12,23 +12,23 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# import dotenv
+import dotenv
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-# dotenv_file = os.path.join(BASE_DIR, ".env")
-# if os.path.isfile(dotenv_file):
-#     dotenv.load_dotenv(dotenv_file)
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+# SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -147,7 +147,6 @@ MEDIA_URL = '/media/'
 
 # GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-print(GOOGLE_API_KEY)
 
 
 LOCATION_FIELD = {
